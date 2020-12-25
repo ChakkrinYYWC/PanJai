40b70c3747fa2c749ee9cda273919b8b0d1ce731
TBoonyapat <boonyapat.tawan@gmail.com>
2020-12-25 15:35:01 +0700

    tawan

--- panjai/src/App.js
+++ panjai/src/App.js
@@ -1,25 +1,23 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  getG(){
+    return 'Hello';
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>{this.getG()}</h1>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
