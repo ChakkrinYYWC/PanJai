47c1da1f51b38f591ba2988da02bb82be007f3eb
LAPTOP-N1SNPBC7\user <hentaidasu@gmail.com>
2020-12-25 22:44:43 +0700

    roong

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ const   express = require("express"),
         cors = require('cors'),
         axios = require('axios');
 
+const   user = require('./model/user');
+
 const   app = express();
 app.use(cors())
 app.use(bodyParser.json())
 app.use(methodOverride("_method"));
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(require('express-session')({
     secret: 'SE101',
     resave: false,
@@ -20,27 +24,57 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
-//passport.use(new passportLocal(User.authenticate()));
-//passport.serializeUser(User.serializeUser());
-//passport.deserializeUser(User.deserializeUser());
+mongoose.connect('mongodb+srv://Roong:rung241142@cluster0.txha8.mongodb.net/Cluster0?retryWrites=true&w=majority', {useNewUrlParser: true,  useUnifiedTopology: true });
+
+passport.use(new passportLocal(user.authenticate()));
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
 
 
-app.post('/signin/facebook', async (req, res) => {
-    console.log('Request -->', req.body.user)
+// app.post('/signin/facebook', async (req, res) => {
+//     console.log('Request -->', req.body.user)
   
-    try {
-      const response = await axios({
-        method: 'get',
-        url: `https://graph.facebook.com/v6.0/oauth/access_token?grant_type=fb_exchange_token&client_id=771047717101312&client_secret=94480ba4cd991ea93c6c8c87de138c03&fb_exchange_token=${req.body.user.accessToken}`
-      })
-       
-      const result = response.data
-      console.log('Result -->', result)
+//     try {
+//       const response = await axios({
+//         method: 'get',
+//         url: `https://graph.facebook.com/v6.0/oauth/access_token?grant_type=fb_exchange_token&client_id=771047717101312&client_secret=94480ba4cd991ea93c6c8c87de138c03&fb_exchange_token=${req.body.user.accessToken}`
+//       })
+      
+//       const result = response.data
+//       console.log('Result -->', result)
   
-      // If (result) --> process signup (new user) / signin (exiting user)
-    } catch (error) {}
+//       // If (result) --> process signup (new user) / signin (exiting user)
+//     } catch (error) {}
+// })
+
+app.get('/', (req, res)=>{
+  let response = user.username;
+  res.send(response);
 })
+// app.post("/user", function(req, res){
+//   if(req.body.password != req.body.CPassword){
+//       console.log("confirm password incorrect")
+//       return res.redirect('/')
+//   }
+//   user.register(new user({username: req.body.Username}), req.body.password,function(error, user){
+//       if(error){
+//           console.log(error);
+//           return res.render('call back register')
+//       }
+      
+//       passport.authenticate('local')(req,res,function(){
+//           //req.flash('success','Welcome to our website ,'+ user.username)
+//           res.redirect('/')
+//       })
+//   })
+// })
 
-app.listen(8000,function(req,res){
+app.listen(3001,function(req,res){
     console.log('Panjai has started!');
 });
+
+// user.create({
+//   username : 'roong',
+//   password : 12345
+// }
+// )
