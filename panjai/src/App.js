aff45a678e619ecf62143a42579bcfa44c9bab33
TBoonyapat <boonyapat.tawan@gmail.com>
2020-12-25 22:45:38 +0700

    tawan

--- panjai/src/App.js
+++ panjai/src/App.js
@@ -5,33 +5,6 @@ import React from 'react';
 import './App.css';
 
 
-function App() {
-  const signUserIn = async response => {
-    console.log('Res -->', response)
-    const { name, email, accessToken, userID } = response
-    const user = { name, email, accessToken, userId: userID }
 
-    await axios({
-      method: 'post',
-      url: 'http://localhost:8000/signin/facebook',
-      data: {
-        user
-      }
-    })
-  }
-
-  return (
-    <div className='App'>
-      <div>
-        <FacebookLogin
-          appId='771047717101312'
-          fields='name,email'
-          scope='public_profile, email'
-          callback={signUserIn}
-        />
-      </div>
-    </div>
-  )
-}
 
 export default App;
