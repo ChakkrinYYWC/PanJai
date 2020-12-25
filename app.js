cff70eab5929ee12d626b1ca81db11e7486a74cb
LAPTOP-N1SNPBC7\user <hentaidasu@gmail.com>
2020-12-25 18:16:20 +0700

    roong

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ const   express = require("express"),
         passport = require('passport'),
         passportLocal = require('passport-local'),
         passportLocalMongoose = require('passport-local-mongoose'),
-        methodOverride = require('method-override'),
-        cors = require('cors'),
-        axios = require('axios'),
-        indexRoutes = require('./routes/index');
+        methodOverride = require('method-override');
+const { monitorEventLoopDelay } = require("perf_hooks");
 
 const   app = express();
-app.use(cors())
 app.use(methodOverride("_method"));
 app.use(require('express-session')({
     secret: 'SE101',
@@ -18,29 +15,8 @@ app.use(require('express-session')({
     saveUninitialized: false
 }));
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session()); 
 
-//passport.use(new passportLocal(User.authenticate()));
-//passport.serializeUser(User.serializeUser());
-//passport.deserializeUser(User.deserializeUser());
-
-app.use('/',indexRoutes);
-
-app.post('/signin/facebook', async (req, res) => {
-    console.log('Request -->', req.body.user)
-  
-    try {
-      const response = await axios({
-        method: 'get',
-        url: `https://graph.facebook.com/v6.0/oauth/access_token?grant_type=fb_exchange_token&client_id=771047717101312&client_secret=94480ba4cd991ea93c6c8c87de138c03&fb_exchange_token=${req.body.user.accessToken}`
-      })
-       
-      const result = response.data
-      console.log('Result -->', result)
-  
-      // If (result) --> process signup (new user) / signin (exiting user)
-    } catch (error) {}
-  })
 
 app.listen(8000,function(req,res){
     console.log('EduTarot has started!');
