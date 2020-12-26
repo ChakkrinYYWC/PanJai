514a8b814563718a1591d1a1c13e524fd3543871
TBoonyapat <boonyapat.tawan@gmail.com>
2020-12-26 15:58:53 +0700

    tawan

--- model/postPanjai.js
+++ model/postPanjai.js
@@ -3,6 +3,6 @@ const   mongoose = require('mongoose')
 var PostPanjai = mongoose.model('PostPanjai',{
     title : String,
     message : String,
-})
+},'PostPanjai')
 
 module.exports = { PostPanjai }
